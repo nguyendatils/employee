7c1d7ab99d61fc38d0cb4d753023af5ab3af9dd5
nguyendatils <61770569+nguyendatils@users.noreply.github.com>
2021-07-19 15:47:53 +0700

    login and registration update-2

---
+++ src/main/java/com/viettel/employee/service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.viettel.employee.service;
+
+import com.viettel.employee.dto.CustomUserDetail;
+import com.viettel.employee.model.User;
+import com.viettel.employee.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetail(user);
+    }
+}
